f983037634473e8950a51d89ad55aca3519a68a2
Julio Schilders <julio.schilders@gmail.com>
2019-09-12 21:47:56 +0200

    Using React with all its functionality now

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.sass';
 
-// File system & OS imports
-import fs from 'fs';
-import os from 'os';
-import child_process from 'child_process';
-const input = document.querySelector('input[type=text]');
+// window.require because we're using React with Webpack
+const fs = window.require('fs');
+const os = window.require('os');
+const child_process = window.require('child_process');
 
 export default function App() {
+  const [input, setInput] = useState('');
+
   return (
     <div className="container">
       <h1>Youtube Downloader</h1>
       <div className="user-options">
         <div className="user-input">
-          <label htmlFor="music-input">Enter songs:</label>
-          <input type="text" name="music-input" />
+          <label htmlFor="music-input">Enter song(s):</label>
+          <input
+            type="text"
+            name="music-input"
+            value={input}
+            placeholder="Enter YouTube video URL here..."
+            onChange={event => setInput(event.target.value)}
+          />
         </div>
         <div className="button-wrapper">
           <button
             className="add-to-list"
             onClick={e => {
-              fs.appendFile('../songs.txt', input.value + os.EOL, err => {
+              fs.appendFile('./songs.txt', input + os.EOL, err => {
                 if (err) throw err;
               });
+              setInput('');
             }}
           >
             Add to download list
           </button>
           <button
             className="download-button"
-            onClick={e => {
-              child_process.execSync('python3 downloader.py songs.txt');
-            }}
+            onClick={e =>
+              child_process.execSync('python3 downloader.py songs.txt')
+            }
           >
             Download Songs
           </button>
