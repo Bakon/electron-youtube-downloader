40ca04f12a58925ac741c6ba55d0865c4ac04372
Julio Schilders <julio.schilders@gmail.com>
2019-09-12 18:07:58 +0200

    Final push before importing react

--- downloader.py
+++ downloader.py
@@ -4,7 +4,6 @@ from sys import argv
 import os
 import youtube_dl
 
-
 class Logger(object):
   def debug(self, msg):
     pass
@@ -15,12 +14,11 @@ class Logger(object):
   def error(self, msg):
     print(msg)
 
-
 def progress_checker(download):
   if download['status'] == 'finished':
     print('Done downloading: \n  ' + download['filename'])
 
-
+file = '../' + argv[1]
 download_options = {
   'format': 'best',
   'outtmpl': '%(title)s.%(ext)s',
@@ -29,16 +27,17 @@ download_options = {
   'progress_hooks': [progress_checker],
 }
 
-
-if os.path.exists('Music'):
-  os.chdir('Music')
-else:
+if not os.path.exists('Music'):
   os.mkdir('Music')
+  
+os.chdir('Music')
 
 # Initializes the youtubeDL class as 'dl'
 with youtube_dl.YoutubeDL(download_options) as dl:
-  with open('../' + argv[1], 'r') as file:
-    for song_url in file:
+  with open(file, 'r') as file_in:
+    for song_url in file_in:
       song = dl.download([song_url])
-
+    data = file_in.read().splitlines(True)
+  with open(file, 'w') as file_out:
+    file_out.writelines(data[1:])
 
